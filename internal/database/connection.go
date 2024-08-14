dd9e98923e8e35a05e34165f08febb556c133cd2
denisbuarque <denisprogramadorweb@gmail.com>
2024-08-14 09:49:33 -0300

    feat: buscando post por id

--- internal/database/connection.go
+++ internal/database/connection.go
@@ -20,11 +20,12 @@ func main() {
 	fmt.Println("Conectado ao banco de dados com sucesso.")
 	defer conn.Close(context.Background())
 
-	createTable()
-	insertPost()
+	//createTable()
+	//insertPost()
+	postId()
 }
 
-func createTable() {
+/*func createTable() {
 	query := `
 		CREATE TABLE IF NOT EXISTS posts (
 			id SERIAL PRIMARY KEY,
@@ -37,17 +38,33 @@ func createTable() {
 		panic(err)
 	}
 	fmt.Println("Tabela posts criada com sucesso.")
-}
+}*/
 
-func insertPost() {
-	title := "Post 1"
-	content := "Conteúdo do post 1"
+/*func insertPost() {
+	title := "Post 3"
+	content := "Conteúdo do post 3"
 	author := "Denis"
 
-	query := fmt.Sprintf(`INSERT INTO (title, content, author) VALUES ('%s', '%s', '%s')`, title, content, author)
-	_, err := conn.Exec(context.Background(), query)
+	query := `INSERT INTO posts (title, content, author) VALUES ($1, $2, $3)`
+	_, err := conn.Exec(context.Background(), query, title, content, author)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Post inserido com sucesso.")
+}*/
+
+func postId() {
+	id := 1
+	var title, content, author string
+	query := "SELECT title, content, author FROM posts WHERE id=$1"
+	row := conn.QueryRow(context.Background(), query, id)
+	err := row.Scan(&title, &content, &author)
+	if err == pgx.ErrNoRows {
+		fmt.Println("Post not found for id: ", id)
+		return
+	}
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Titulo: %s, Conteúdo: %s, Autor: %s \n", title, content, author)
 }
